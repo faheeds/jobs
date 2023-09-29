7fb1cfb830cee15153bfc05e0401012f3d022e80
faheeds <42218586+faheeds@users.noreply.github.com>
2023-09-29 08:06:29 -0700

    Update jobs.py

--- jobs.py
+++ jobs.py
@@ -1,39 +1,43 @@
 import streamlit as st
 import requests 
+from oauth2client.client import GoogleCredentials
+
+def get_access_token():
+    credentials = GoogleCredentials.from_stream(st.secrets["client_secret_path"])
+    credentials = credentials.create_scoped(['https://www.googleapis.com/auth/cloud-platform'])
+    return credentials.get_access_token().access_token
 
 st.title('Job Search')
 
 companies = st.multiselect('Select Companies', ['Google', 'Facebook', 'Microsoft'])
 
-API_KEY = 'AIzaSyAuFskRWKMFZailNaCf02sAfnbqz9IP3JE'  # Consider moving this to a secure location
-
 url = 'https://jobs.googleapis.com/v2/jobs/search' 
 
-if companies:
-  params = {
-    'q': 'python developer', 
-    'companyNames': ','.join(companies),  
-    'location': 'San Francisco, CA',
-    'key': API_KEY
-  }
-else:
-  params = {
-    'q': 'python developer',  
-    'location': 'San Francisco, CA',
-    'key': API_KEY
-  }
-  
-try:
-    response = requests.get(url, params=params)
-    response.raise_for_status()  # Raise an HTTPError if the HTTP request returned an unsuccessful status code
-
-    data = response.json()
+if st.button("Search Jobs"):
     
-    if 'jobs' in data:
-        for job in data['jobs']:
-            st.write(job['title'], job['company'], job['location'])
+    headers = {
+        'Authorization': f'Bearer {get_access_token()}'
+    }
+    
+    if companies:
+        params = {
+            'q': 'python developer', 
+            'companyNames': ','.join(companies),  
+            'location': 'San Francisco, CA'
+        }
     else:
-        st.write("No jobs found.")
+        params = {
+            'q': 'python developer',  
+            'location': 'San Francisco, CA'
+        }
+      
+    try:
+        response = requests.get(url, headers=headers, params=params)
+        response.raise_for_status()
 
-except requests.exceptions.RequestException as e:
-    st.write(f"An error occurred: {e}")
+        data = response.json()
+        
+        if 'jobs' in data:
+            for job in data['jobs']:
+                st.write(job['title'], job['company'], job['location'])
+       
