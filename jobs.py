132c46007fc875f25c5a4eef7b4bb1c1b617bd5f
faheeds <42218586+faheeds@users.noreply.github.com>
2023-09-29 09:13:02 -0700

    Update jobs.py

--- jobs.py
+++ jobs.py
@@ -3,7 +3,20 @@ import requests
 from oauth2client.client import GoogleCredentials
 
 def get_access_token():
-    credentials = GoogleCredentials.from_stream(st.secrets["client_secret_path"])
+    credentials_data = {
+        "type": "service_account",
+        "project_id": st.secrets["google"]["project_id"],
+        "private_key_id": st.secrets["google"]["private_key_id"],
+        "private_key": st.secrets["google"]["private_key"].replace("\\n", "\n"),
+        "client_email": st.secrets["google"]["client_email"],
+        "client_id": st.secrets["google"]["client_id"],
+        "auth_uri": st.secrets["google"]["auth_uri"],
+        "token_uri": st.secrets["google"]["token_uri"],
+        "auth_provider_x509_cert_url": st.secrets["google"]["auth_provider_x509_cert_url"],
+        "client_x509_cert_url": st.secrets["google"]["client_x509_cert_url"]
+    }
+    
+    credentials = GoogleCredentials.from_stream(credentials_data)
     credentials = credentials.create_scoped(['https://www.googleapis.com/auth/cloud-platform'])
     return credentials.get_access_token().access_token
 
