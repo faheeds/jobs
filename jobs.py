b0c4b2536b7922daac9c9f87c09ce3292e7aa7a4
faheeds <42218586+faheeds@users.noreply.github.com>
2023-09-29 07:54:48 -0700

    Update jobs.py

--- jobs.py
+++ jobs.py
@@ -25,4 +25,15 @@ else:
   
 try:
     response = requests.get(url, params=params)
-    response.raise_for
+    response.raise_for_status()  # Raise an HTTPError if the HTTP request returned an unsuccessful status code
+
+    data = response.json()
+    
+    if 'jobs' in data:
+        for job in data['jobs']:
+            st.write(job['title'], job['company'], job['location'])
+    else:
+        st.write("No jobs found.")
+
+except requests.exceptions.RequestException as e:
+    st.write(f"An error occurred: {e}")
